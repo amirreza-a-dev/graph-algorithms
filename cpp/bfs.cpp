5fc9b65d606d70df09a580f3966f7f7cf191b543
amirreza-a-dev <amirreza02005@gmail.com>
2026-09-05 15:21:13 +0330

    Use adjacency list in BFS

--- cpp/bfs.cpp
+++ cpp/bfs.cpp
@@ -5,67 +5,68 @@
 
 using namespace std;
 
-vector<int> neighborhood(int vertex, const vector<vector<int>>& edges, const vector<bool>& marked)
+vector<vector<int>> get_adj(const vector<int>& vertices, const vector<vector<int>>& edges)
 {
-    vector<int> neighbors;
-    for (const vector<int>& i : edges)
+    int vertices_size=static_cast<int>(vertices.size());
+    vector<vector<int>> adjacents(static_cast<size_t>(vertices_size));
+    int i;
+    for(i=0;i<vertices_size;i++)
     {
-        if (i[0]==vertex)
+        for(const vector<int>& j:edges)
         {
-            if (!(marked[static_cast<size_t>(i[1])]))
-            {
-                neighbors.push_back(i[1]);
+            if (j[0]==i)
+            {        
+                adjacents[static_cast<size_t>(i)].push_back(j[1]);    
             }
-        }
-        if (i[1]==vertex)
-        {
-            if (!(marked[static_cast<size_t>(i[0])]))
+            if (j[1]==i)
             {
-                neighbors.push_back(i[0]);
+                adjacents[static_cast<size_t>(i)].push_back(j[0]);
             }
         }
     }
-    return neighbors;
+    return adjacents;
 }
 
 
 vector<int> bfs(const vector<int>& vertices, const vector<vector<int>>& edges, int start, int target)
 {
+    if (start==target)
+    {
+        return {start};
+    }
     queue<int> q;
     vector<int> parent(vertices.size(), -1);
     vector<bool> marked(vertices.size(), false);
     vector<int> result;
     vector<int> no_path_message{-1};
+    int current;
+    vector<vector<int>> adjacents=get_adj(vertices, edges);
 
-    if (start==target)
-    {
-        vector<int> s;
-        s.push_back(start);
-        return s;
-    }
     marked[static_cast<size_t>(start)]=true;
     q.push(start);
 
-    while(!q.empty())
+    while (!q.empty())
     {
-        int current=q.front();
-        vector<int> neighbors=neighborhood(current, edges, marked);
-        for (int i: neighbors)
+        current=q.front();
+        for (int i: adjacents[static_cast<size_t>(current)])
         {
-            parent[static_cast<size_t>(i)]=current;
-            marked[static_cast<size_t>(i)]=true;
-            if (i==target)
+            if (!marked[static_cast<size_t>(i)])
             {
-                int subject=i;
-                while(subject!=-1)
+                marked[static_cast<size_t>(i)]=true;
+                parent[static_cast<size_t>(i)]=current;
+                q.push(i);
+                if (i==target)
                 {
-                    result.push_back(subject);
-                    subject=parent[static_cast<size_t>(subject)];
+                    result.push_back(i);
+                    while(current!=-1)
+                    {
+                        result.push_back(current);
+                        current=parent[static_cast<size_t>(current)];
+                    }
+                    reverse(result.begin(), result.end());
+                    return result;
                 }
-                reverse(result.begin(), result.end());
-                return result;
             }
-            q.push(i);
         }
         q.pop();
     }
